e326fa23ef8c82348d2b332bb131a1add4dd74e3
Marc <mdafeldecker@gmail.com>
2022-05-14 14:48:27 +0200

    add User test suite & test fixtures

---
+++ src/tests/User.spec.ts
@@ -0,0 +1,64 @@
+import supertest from 'supertest';
+import * as UserService from '../service/user.service';
+import createServer from '../utils/createServer';
+import { userInput, userInputIncomplete, userInputInvalidPasswords, userPayload } from './_test_fixtures';
+
+const app = createServer();
+
+describe('User', () => {
+	describe('user registration', () => {
+		describe('given the registration data is complete & valid', () => {
+			it('should call createUserService and return the user payload', async () => {
+				const createUserServiceMock = jest
+					.spyOn(UserService, 'createUser')
+					// @ts-ignore
+					.mockReturnValueOnce(userPayload);
+
+				const { body } = await supertest(app).post('/api/v1/users').send(userInput).expect(200);
+
+				expect(body).toEqual(userPayload);
+				expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
+			});
+		});
+
+		describe('given the registration data is incomplete ', () => {
+			it('should not call createUserService and return status 400 bad request', async () => {
+				const createUserServiceMock = jest
+					.spyOn(UserService, 'createUser')
+					// @ts-ignore
+					.mockReturnValueOnce(userPayload);
+
+				const { body } = await supertest(app).post('/api/v1/users').send(userInputIncomplete).expect(400);
+
+				expect(createUserServiceMock).not.toHaveBeenCalled();
+			});
+		});
+
+		describe('given the passwords for registration do not match', () => {
+			it('should not call createUserService and return status 400 bad request', async () => {
+				const createUserServiceMock = jest
+					.spyOn(UserService, 'createUser')
+					// @ts-ignore
+					.mockReturnValueOnce(userPayload);
+
+				const { body } = await supertest(app).post('/api/v1/users').send(userInputInvalidPasswords).expect(400);
+
+				expect(createUserServiceMock).not.toHaveBeenCalled()
+			});
+		});
+
+		describe('given the user service throws an error', () => {
+			it('should return a 409 status', async () => {
+				const createUserServiceMock = jest
+					.spyOn(UserService, 'createUser')
+					.mockRejectedValue('ooops ..');
+
+				const { statusCode } = await supertest(app).post('/api/v1/users').send(userInput);
+
+				expect(statusCode).toBe(409);
+
+				expect(createUserServiceMock).toHaveBeenCalled();
+			});
+		});
+	});
+});
---
+++ src/tests/_test_fixtures.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userId = new mongoose.Types.ObjectId().toString();
+
+export const userInput = {
+  email: 'test@test.de',
+  name: 'Hans Mustermann',
+  password: 'asdf1234',
+  passwordConfirmation: 'asdf1234',
+};
+
+export const userPayload = {
+	_id: userId,
+	email: 'test@test.de',
+	name: 'Hans Mustermann',
+};
+
+export const userInputInvalidPasswords = {
+  email: 'test@test.de',
+  name: 'Hans Mustermann',
+  password: 'asdf1234',
+  passwordConfirmation: 'asdf9876',
+};
+
+export const userInputIncomplete = {
+  name: 'Hans Mustermann',
+  password: 'asdf1234',
+  passwordConfirmation: 'asdf9876',
+};
