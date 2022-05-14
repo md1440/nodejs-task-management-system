e4ca9df4878ab4093c432bc5ebee2a7f422750e4
Marc <mdafeldecker@gmail.com>
2022-05-14 16:09:14 +0200

    update configs with rsa keys

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,6 @@ export default {
 	port: 3000,
 	dbUri: 'mongodb://localhost:27017/nodejs-task-management-system',
 	saltRounds: 12,
+	publicKey: process.env.PUBLIC_KEY,
+	privateKey: process.env.PRIVATE_KEY,
 };
