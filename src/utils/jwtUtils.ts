2e3497ff3a1271cfb5ccbc3b3755b4a4853088f2
Marc <mdafeldecker@gmail.com>
2022-05-15 10:43:20 +0200

    add src/utils/jwtUtils with signJwt & verifyJwt

---
+++ src/utils/jwtUtils.ts
@@ -0,0 +1,29 @@
+import config from 'config';
+import jwt, { SignOptions } from 'jsonwebtoken';
+
+const publicKey = config.get<string>('publicKey');
+const privateKey = config.get<string>('privateKey');
+
+export function signJwt(payload: Object, options: SignOptions | undefined) {
+	return jwt.sign(payload, privateKey, {
+		...(options && options),
+		algorithm: 'RS256',
+	});
+}
+
+export function verifyJwt(token: string) {
+	try {
+		const decoded = jwt.verify(token, publicKey);
+		return {
+			valid: true,
+			expired: false,
+			decoded,
+		};
+	} catch (err: any) {
+    return {
+      valid: false,
+      expired: err.message === 'jwt token expired',
+      decoded: null,
+    }
+  }
+}
