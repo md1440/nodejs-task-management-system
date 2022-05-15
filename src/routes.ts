b4b097c6f1ecfa305b297ddfcf102d768e21ccd4
Marc <mdafeldecker@gmail.com>
2022-05-15 13:10:34 +0200

    implement GET /sessions/ routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express';
-import { createUserSessionHandler } from './controller/session.controller';
+import { createUserSessionHandler, getUserSessionHandler } from './controller/session.controller';
 import { createUserHandler } from './controller/user.controller';
+import requireUser from './middleware/requireUser';
 import validateRequest from './middleware/validateRequest';
 import { createSessionSchema } from './schema/Session.schema';
 import { createUserSchema } from './schema/User.schema';
@@ -9,6 +10,8 @@ function routes(app: Express) {
 	app.post('/api/v1/users', validateRequest(createUserSchema), createUserHandler);
 
 	app.post('/api/v1/sessions', validateRequest(createSessionSchema), createUserSessionHandler);
+
+	app.get('/api/v1/sessions', requireUser, getUserSessionHandler )
 }
 
 export default routes;
