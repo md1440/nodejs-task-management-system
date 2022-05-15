0a8b77a07a0b4e465c22158146ea158f617d808d
Marc <mdafeldecker@gmail.com>
2022-05-15 16:11:53 +0200

    add POST /tasks to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express';
 import { createUserSessionHandler, getUserSessionHandler } from './controller/session.controller';
+import { createTaskHandler } from './controller/task.controller';
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validateRequest from './middleware/validateRequest';
 import { createSessionSchema } from './schema/Session.schema';
+import { createTaskSchema } from './schema/Task.schema';
 import { createUserSchema } from './schema/User.schema';
 
 function routes(app: Express) {
@@ -11,7 +13,9 @@ function routes(app: Express) {
 
 	app.post('/api/v1/sessions', validateRequest(createSessionSchema), createUserSessionHandler);
 
-	app.get('/api/v1/sessions', requireUser, getUserSessionHandler )
+	app.get('/api/v1/sessions', requireUser, getUserSessionHandler);
+
+	app.post('/api/v1/tasks', [requireUser, validateRequest(createTaskSchema)], createTaskHandler);
 }
 
 export default routes;
