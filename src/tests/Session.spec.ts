a2cb864ac5355a2ec40639ca2b2aec46d37cc791
Marc <mdafeldecker@gmail.com>
2022-05-15 13:08:23 +0200

    add test for GET sessions tp Session.soec

--- src/tests/Session.spec.ts
+++ src/tests/Session.spec.ts
@@ -1,12 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import supertest from 'supertest';
 import { createUserSessionHandler } from '../controller/session.controller';
 import * as SessionService from '../service/session.service';
+import { createSession } from '../service/session.service';
 import * as UserService from '../service/user.service';
+import { createUser } from '../service/user.service';
 import createServer from '../utils/createServer';
-import { sessionPayload, userPayload } from './_test_fixtures';
+import { signJwt } from '../utils/jwtUtils';
+import { sessionPayload, userInput, userPayload } from './_test_fixtures';
 
 const app = createServer();
 
 describe('Session Endpoint Tests', () => {
+	beforeAll(async () => {
+		const mongoServer = await MongoMemoryServer.create();
+
+		await mongoose.connect(mongoServer.getUri());
+	});
+
+	afterAll(async () => {
+		await mongoose.disconnect();
+		await mongoose.connection.close();
+	});
+
 	describe('given the username and password are valid', () => {
 		it('should return a signed accessToken & refreshToken', async () => {
 			jest
@@ -39,6 +56,34 @@ describe('Session Endpoint Tests', () => {
 				accessToken: expect.any(String),
 				refreshToken: expect.any(String),
 			});
+		});
+	});
+
+	describe('given the accessToken is valid & user is logged in', () => {
+		it('should return all valid sessions', async () => {
+			const user = await createUser(userInput);
+			const session = await createSession(user._id.toString(), 'PostmanRuntime/7.28.4');
+			const jwt = signJwt({ userPayload, _id: `${user._id}` });
+
+			const { body } = await supertest(app)
+				.get('/api/v1/sessions')
+				.set('Authorization', `Bearer ${jwt}`)
+				.expect(200);
+
+			expect(body).toHaveLength(1);
+			expect(body).toEqual(
+				expect.arrayContaining([
+					expect.objectContaining({
+						_id: expect.any(String),
+						user: expect.any(String),
+						valid: true,
+						userAgent: 'PostmanRuntime/7.28.4',
+						createdAt: expect.any(String),
+						updatedAt: expect.any(String),
+						__v: 0,
+					}),
+				])
+			);
 		});
 	});
 });
