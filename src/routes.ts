603229c7f7b5a0ef0e9be2f9a3574805e54725f2
Marc <mdafeldecker@gmail.com>
2022-05-15 09:57:46 +0200

    add  POST /sessions route for login

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Express } from 'express';
+import { createUserSessionHandler } from './controller/session.controller';
 import { createUserHandler } from './controller/user.controller';
 import validateRequest from './middleware/validateRequest';
+import { createSessionSchema } from './schema/Session.schema';
 import { createUserSchema } from './schema/User.schema';
 
 function routes(app: Express) {
 	app.post('/api/v1/users', validateRequest(createUserSchema), createUserHandler);
+
+	app.post('/api/v1/sessions', validateRequest(createSessionSchema), createUserSessionHandler);
 }
 
 export default routes;
