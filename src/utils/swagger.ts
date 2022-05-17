5ee1ba2ecf26e2e1f6a3aa8807bbe992c1bfb63b
Marc <mdafeldecker@gmail.com>
2022-05-17 14:28:06 +0200

    fix typo in swagger.ts

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -34,7 +34,7 @@ const swaggerSpec = swaggerJsdoc(options);
 function swaggerDocs(app: Express, port: number, address: string) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-  app.get('docs.json', (req: Request, res: Response) => {
+  app.get('/docs.json', (req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   })
